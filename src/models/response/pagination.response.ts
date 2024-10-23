75669876b00c794e83a74a074532b7c43c86cb5c
Devin Anugrah <devinanugrahp27@gmail.com>
2024-10-24 00:35:16 +0700

    fix: add constructor to pagination response

--- src/models/response/pagination.response.ts
+++ src/models/response/pagination.response.ts
@@ -1,6 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class PaginationResponse<T> {
+  constructor(
+    message: string,
+    data: T[],
+    metadata: {
+      page: number;
+      size: number;
+      totalData: number;
+      totalPage: number;
+    },
+  ) {
+    this.message = message;
+    this.data = data;
+    this.metadata = metadata;
+  }
   @ApiProperty()
   message: string;
 
